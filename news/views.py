a1998e65e514e2ae80f1b15c2ce2e8e7b2ad1ffa
DICKQI <dickqi87@gmail.com>
2018-04-04 12:30:20 +0800

    前台大致框架

--- news/views.py
+++ news/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+# 首页
+def index(requests):
+    return HttpResponse(u"欢迎光临北师珠最大的万能平台AlgYun")
+
+def column_detail(requests, column_slug):
+    return HttpResponse('column slug ' + column_slug)
+
+def article_detail(requests, article_slug):
+    return HttpResponse('article slug ' + article_slug)
