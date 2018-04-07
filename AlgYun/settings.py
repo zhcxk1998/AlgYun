50cc339d54efb5ce921bbde9ff18d6b692503edd
DESKTOP-ASTA59B\DICKQI <dickqi87@gmail.com>
2018-04-07 23:23:54 +0800

    DEBUG改为false

--- AlgYun/settings.py
+++ AlgYun/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1wg=lu^czfw&exoqa@7dp!k(+r3f=!fgi=_8d_-(gihzh0b=rl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
